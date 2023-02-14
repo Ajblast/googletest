c0a0a8d3efb90fa1472f8c72f2635f34ccf9698b
Ajblast <46971018+Ajblast@users.noreply.github.com>
2023-02-13 22:50:41 -0500

    Update premake

--- premake5.lua
+++ premake5.lua
@@ -14,7 +14,55 @@ project "GoogleTest"
 	{
 		"googletest/include/**",
 		"googletest/src/gtest-all.cc",
+	}
+
+	-- Define the following
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS"
+	}
+
+	-- Include directories
+	includedirs
+	{
+		"googletest",
+		"googletest/include",
+	}
+
+	-- When the system is windows
+	filter "system:windows"
+		systemversion "latest"
+
+	-- When the configuration is debug
+	filter "configurations:Debug"
+		runtime "Debug"				-- Debug runtime optimization
+		symbols "on"				-- Generate debug symbols
+		
+	-- When the configuration is Release
+	filter "configurations:Release"
+		runtime "Release"			-- Release runtime optimization
+		optimize "on"				-- Optimize the project (Balanced)
+		
+project "GoogleTestMain"
+	kind "StaticLib"	-- SharedLib for .lib
+	language "C++"		-- C++
+	cppdialect "C++20"	-- C++17
+	staticruntime "on"	-- MultiThreaded
+
+	targ = "..\\..\\bin\\" .. outputdir .. "\\" .. "GoogleTestMain"
+	targetdir (targ)
+	out = "..\\..\\bin-int\\" .. outputdir .. "\\" .. "GoogleTestMain"
+	objdir (out)
+
+	-- Files that should be included into this project
+	files
+	{
 		"googletest/src/gtest_main.cc",
+	}
+	
+	links
+	{
+		"GoogleTest"
 	}
 
 	-- Define the following
